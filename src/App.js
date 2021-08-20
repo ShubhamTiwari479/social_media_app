0ef97b5b7ce29e3f7b1574249624258c638a0506
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2021-08-20 20:51:59 +0530

    login and log out functions included

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+//import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
+import SignIn from './components/Signin';
+import PrimarySearchAppBar from './components/navBar';
+import ImgMediaCard from './components/massage';
+import Signup from './components/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Route exact path="/">
+          <PrimarySearchAppBar />
+        </Route>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route exact path="/massage">
+          <ImgMediaCard />
+        </Route>
+        <Route exact path="/Signup">
+          <Signup />
+        </Route>
+      </Router>
+
     </div>
   );
 }
